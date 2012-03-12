d24a1ee2f3a78a62ea3cf4e4a2c13ff2eaaf9d0c
Shashank Khandelwal <shrew@alumni.cs.utexas.edu>
2012-03-12 13:26:34 -0400

    Logging data out

--- geosera/management/commands/batch_reverse_geocode.py
+++ geosera/management/commands/batch_reverse_geocode.py
@@ -1,4 +1,7 @@
 import csv	
+import simplejson
+import logging
+import logging.handlers
 
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.gis.geos import Point
@@ -18,8 +21,23 @@ class Command(BaseCommand):
 
 	def handle(self, *args, **options):
 		point_file_name = args[0]
-
 		point_file_reader = csv.reader(open(point_file_name, 'rb'), delimiter='\t')
+
+		#setup the data logger
+		data_logger = logging.getLogger('DataLogger')
+		data_logger.setLevel(logging.INFO)
+		data_handler = logging.handlers.TimedRotatingFileHandler('data.log', 'H', 1)
+		data_handler.setFormatter(logging.Formatter('%(asctime)s%(msecs)d|%(message)s', datefmt='%Y%m%d%H%M%S'))
+		data_logger.addHandler(data_handler)
+
+		#setup the application logger
+		logger = logging.getLogger('AppLogger')
+		logger.setLevel(logging.INFO)
+
+		#Rotate the application log, once at midnight
+		logging_handler = logging.handlers.TimedRotatingFileHandler('app.log', 'midnight', 1)
+		logging_handler.setFormatter(logging.Formatter('%(asctime)s%(msecs)d|%(message)s', datefmt='%Y%m%d%H%M%S'))
+		logger.addHandler(logging_handler)
 
 		for row in point_file_reader:
 			tweet_id = row[0]
@@ -30,9 +48,10 @@ class Command(BaseCommand):
 			counties = CountyBoundary.objects.filter(geom__contains=p)
 
 			response_data = {}
+			response_data['tweet_id'] = tweet_id
 
 			if len(counties) > 0:
-				response_data['county'] = counties[0].namelsad10
+				response_data['county'] = counties[0].name10
 				response_data['county_fips'] = counties[0].geoid10
 			else:
 				response_data['county'] = None
@@ -41,6 +60,7 @@ class Command(BaseCommand):
 
 			if len(places) > 0:
 				response_data['place'] = places[0].namelsad10
+				response_data['place_fips'] = places[0].geoid10
 			else:
 				response_data['place'] = None
 
@@ -67,4 +87,5 @@ class Command(BaseCommand):
 			else:
 				response_data['metro'] = None
 
-			print response_data
+			data_logger.info(simplejson.dumps(response_data))
+			logger.info(tweet_id)
