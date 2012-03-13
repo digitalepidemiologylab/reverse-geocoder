f3a91e0ffda1647aac2fa71e3a00552c07f1db85
Shashank Khandelwal <shrew@alumni.cs.utexas.edu>
2012-03-13 17:14:04 -0400

    Fixed SQL scripts to run correctly

--- data_processing/alter-tweets-add-locs.sql
+++ data_processing/alter-tweets-add-locs.sql
@@ -1,6 +1,17 @@
-ALTER TABLE tweets DROP CONSTRAINT `tweets_ibfk_1`;
+ALTER TABLE zctas ADD FOREIGN KEY (state_two_letter) REFERENCES states (two_letter_code);
+ALTER TABLE counties ADD FOREIGN KEY (state_two_letter) REFERENCES states (two_letter_code);
+
+ALTER TABLE tweets ADD COLUMN county_fips int (25);
 ALTER TABLE tweets ADD FOREIGN KEY (county_fips) REFERENCES `counties` (`fips_code`);
+
+ALTER TABLE tweets ADD COLUMN place_fips int (25);
 ALTER TABLE tweets ADD FOREIGN KEY (place_fips) REFERENCES places (fips_code);
+
+ALTER TABLE tweets ADD COLUMN metro_code int(20);
 ALTER TABLE tweets ADD FOREIGN KEY (metro_code) REFERENCES metros (code);
+
+ALTER TABLE tweets ADD COLUMN state_fips int (10);
 ALTER TABLE tweets ADD FOREIGN KEY (state_fips) REFERENCES states (fips_code);
+
+ALTER TABLE tweets ADD COLUMN zcta int(5);
 ALTER TABLE tweets ADD FOREIGN KEY (zcta) REFERENCES zctas (zipcode);
--- data_processing/create-location-tables.sql
+++ data_processing/create-location-tables.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS `counties`;
+DROP TABLE IF EXISTS zctas;
 DROP TABLE IF EXISTS states;
 
 CREATE TABLE states (
@@ -5,32 +7,29 @@ CREATE TABLE states (
 	fips_code int(10) NOT NULL,
 	population int(15),
 	two_letter_code varchar(2),
-	PRIMARY KEY(fips_code), 
+	PRIMARY KEY(fips_code) 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE INDEX state_two ON states (state_two_letter);
+CREATE INDEX state_two ON states (two_letter_code);
 
-DROP TABLE IF EXISTS `counties`;
+ALTER TABLE tweets DROP FOREIGN KEY `tweets_ibfk_1`;
+ALTER TABLE tweets DROP COLUMN county_id;
 
 CREATE TABLE counties (
-	name text,
+	name text NOT NULL,
 	fips_code int(25) NOT NULL,
 	population int(15),
 	state_two_letter varchar(2),
-	KEY `state_two_letter` (`state_two_letter`),
-	CONSTRAINT `cnties_stat_fk1` FOREIGN_KEY (`state_two_letter`) REFERENCES `states` (`two_letter_code`),
-	PRIMARY KEY (fips_code),
-	UNIQUE KEY name (name)
+	PRIMARY KEY (fips_code)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 
 DROP TABLE IF EXISTS places;
 
 CREATE TABLE places (
 	name text NOT NULL,
 	area_type_code varchar(3), 
-	fips_code int(10) NOT NULL
-	PRIMARY KEY(fips_code),
+	fips_code int(25) NOT NULL,
+	PRIMARY KEY(fips_code)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS metros;
@@ -41,12 +40,9 @@ CREATE TABLE metros (
 	PRIMARY KEY(code)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS zctas;
 
 CREATE TABLE zctas (
 	zipcode int(5),
 	state_two_letter VARCHAR(2),
-	KEY `state_two_letter` (`state_two_letter`),
-	CONSTRAINT `zctas_stat_fk1` FOREIGN_KEY (`state_two_letter`) REFERENCES `states` (`two_letter_code`),
 	PRIMARY KEY(zipcode)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
