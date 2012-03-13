cf056bec5ecbc6125323a9c1853b9025b5e13bca
SalatheGroup Production <khandelwal@psu.edu>
2012-03-13 14:52:24 -0400

    Dumped the table schema from socialmedia

---
+++ data_processing/table-schema.sql
@@ -0,0 +1,65 @@
+-- MySQL dump 10.13  Distrib 5.5.21, for osx10.6 (i386)
+--
+-- Host: localhost    Database: localtweets
+-- ------------------------------------------------------
+-- Server version	5.5.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `counties`
+--
+
+DROP TABLE IF EXISTS `counties`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `counties` (
+  `county_id` int(11) NOT NULL DEFAULT '0',
+  `name` text,
+  `state` varchar(3) DEFAULT NULL,
+  PRIMARY KEY (`county_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tweets`
+--
+
+DROP TABLE IF EXISTS `tweets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tweets` (
+  `longitude` double DEFAULT NULL,
+  `latitude` double DEFAULT NULL,
+  `text` text,
+  `source` text,
+  `time` datetime DEFAULT NULL,
+  `tweet_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `user_id` bigint(20) DEFAULT NULL,
+  `county_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`tweet_id`),
+  UNIQUE KEY `tweet_id` (`tweet_id`),
+  KEY `county_id` (`county_id`),
+  CONSTRAINT `tweets_ibfk_1` FOREIGN KEY (`county_id`) REFERENCES `counties` (`county_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-03-13 14:51:37
